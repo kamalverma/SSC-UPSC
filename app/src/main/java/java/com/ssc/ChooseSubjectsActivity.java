d1aef6322703b338d3691f5f80fd4acae6bd0898
Kamal <kamal@ixigo.com>
2016-12-08 15:46:16 +0530

    Initial commit

---
+++ app/src/main/java/java/com/ssc/ChooseSubjectsActivity.java
@@ -0,0 +1,17 @@
+package java.com.ssc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kamal on 12/8/16.
+ */
+
+public class ChooseSubjectsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/main/java/java/com/ssc/ExamsNotificationActivity.java
@@ -0,0 +1,8 @@
+package java.com.ssc;
+
+/**
+ * Created by Kamal on 12/8/16.
+ */
+
+public class ExamsNotificationActivity {
+}
